5633ffa9a3e862fddecd94014f4b5d4aed31065b
mohit sharma <mohitsharma7258@gmail.com>
2025-02-28 12:42:45 +0530

    front

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -8,32 +8,30 @@ import MobileSearchBar from "../MobileSearchBar";
 
 function Header() {
   return (
-    <div className="navbar">
-      <img
-        src="https://i.ibb.co/Nd1Gncm4/Screenshot-2025-02-27-141816-Photoroom.png"
-        alt="logo"
-        className="navbar-logo"
-      />
-      <div className="search-bar">
-        <div className="search-bar-text">Anywhere</div>
-        <div className="search-bar-text">Any Week</div>
-        <div className="search-bar-text2">Add guests</div>
-        <div className="search-icon-div">
-          <SearchRoundedIcon className="search-icon" />
+    <>
+      <div className="navbar">
+        <h3>Rightstay</h3>
+        <div className="search-bar">
+          <div className="search-bar-text">Anywhere</div>
+          <div className="search-bar-text">Any Week</div>
+          <div className="search-bar-text2">Add guests</div>
+          <div className="search-icon-div">
+            <SearchRoundedIcon className="search-icon" />
+          </div>
         </div>
+        <div className="profile-container">
+          <div className="rightstay-your-home">Rightstay your home</div>
+          <div className="rightstay-your-home">
+            <LanguageIcon sx={{ fontSize: "1.3rem" }} />
+          </div>
+          <div className="profile-div">
+            <BasicMenu />
+          </div>
+        </div>
+        <MobileSearchBar />
+        <SimpleBottomNavigation />
       </div>
-      <div className="profile-container">
-        <div className="rightstay-your-home">RightStay your home</div>
-        <div className="rightstay-your-home">
-          <LanguageIcon sx={{ fontSize: "1.3rem" }} />
-        </div>
-        <div className="profile-div">
-          <BasicMenu />
-        </div>
-      </div>
-      <MobileSearchBar />
-      <SimpleBottomNavigation />
-    </div>
+    </>
   );
 }
 
